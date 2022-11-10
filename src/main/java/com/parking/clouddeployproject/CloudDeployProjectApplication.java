f33cb89ef8f567ec099f7f5aea80b71a065e933c
Gabriel Albuquerque <gabriel.albuquerque2001@gmail.com>
2022-11-10 08:06:03 -0300

    Primeiro Commit

---
+++ src/main/java/com/parking/clouddeployproject/CloudDeployProjectApplication.java
@@ -0,0 +1,13 @@
+package com.parking.clouddeployproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudDeployProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudDeployProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/parking/clouddeployproject/CloudDeployProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parking.clouddeployproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudDeployProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
